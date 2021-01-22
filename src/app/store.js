7f374fd79ea8e0e9b80d0df673b0e427e3d0a0d3
gkatai <katai.gergely@gmail.com>
2021-01-22 20:39:55 +0100

    chore(all): hot module reloading for stores added

--- src/app/store.js
+++ src/app/store.js
@@ -6,4 +6,12 @@ const store = configureStore({
   reducer: rootReducer,
 });
 
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV === "development" && module.hot) {
+  // eslint-disable-next-line no-undef
+  module.hot.accept("./rootReducer", () => {
+    store.replaceReducer(rootReducer);
+  });
+}
+
 export default store;
